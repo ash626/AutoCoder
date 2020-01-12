ad45434cb3007ccc6c7d058286dd6f868e655f6f
Ash Wang <ash6260@gmail.com>
2020-01-12 17:11:56 -0500

    Added codemirror

---
+++ src/AutoCoder.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Controlled as CodeMirror} from 'react-codemirror2'
+require('codemirror/theme/dracula.css');
+require('codemirror/mode/css/css.js');
+require('codemirror/lib/codemirror.css');
+class AutoCoder extends Component{
+  constructor(props){
+    super(props)
+    this.state = {css: "autocoder"}
+  }
+
+  render(){
+    return <div id='codemirror-wrapper'>
+      <CodeMirror
+        value={this.state.css}
+        options={{
+          mode: 'css',
+          theme: 'dracula',
+          lineWrapping: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          this.setState({css: value});
+        }}
+      />
+    </div>
+  }
+}
+
+export default AutoCoder;
