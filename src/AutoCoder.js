a49102966f363943a1b4f30cfa386b11589d3184
Ash Wang <ash6260@gmail.com>
2020-01-12 19:14:46 -0500

    Added function to add characters one by one to css attribute in state

--- src/AutoCoder.js
+++ src/AutoCoder.js
@@ -3,12 +3,25 @@ import {Controlled as CodeMirror} from 'react-codemirror2'
 require('codemirror/theme/dracula.css');
 require('codemirror/mode/css/css.js');
 require('codemirror/lib/codemirror.css');
+
+const script= "#codemirror-wrapper{\n  position: absolute;\n  top: 25vh;\n  left: 25vw;\n}";
+
 class AutoCoder extends Component{
   constructor(props){
     super(props)
-    this.state = {css: "autocoder"}
+    this.state = {css: ""}
   }
 
+  componentDidMount(){
+    this.animateWrite(script);
+  }
+
+  animateWrite(script){
+    if (script === "") return;
+    this.setState({css: this.state.css + script.charAt(0)});
+    setTimeout(this.animateWrite.bind(this), 30, script.substring(1, script.length));
+  }
+  
   render(){
     return <div id='codemirror-wrapper'>
       <style dangerouslySetInnerHTML={{__html: this.state.css}}/>
@@ -25,6 +38,7 @@ class AutoCoder extends Component{
       />
     </div>
   }
+
 }
 
 export default AutoCoder;
